23bb4719938f199f250ed924638820da245b09eb
Amanuel <amanuel.demirew@yahoo.com>
2023-11-24 18:23:21 +0300

    package change 3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 # install flask
 
-exec { 'install_flask':
+exec { 'flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
   unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
