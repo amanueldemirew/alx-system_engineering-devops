4b4a4337a743c6dbeb85d25914bf251a4bc0f44e
Amanuel <amanuel.demirew@yahoo.com>
2023-11-24 18:32:39 +0300

    remove

--- 0x0A-configuration_management/install_puppet_lint.pp
+++
@@ -1,6 +0,0 @@
-exec { 'install_puppet_lint':
-  command => '/usr/bin/gem install -v 1.1.0 --no-document puppet-lint',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/gem list -i puppet-lint -v 1.1.0',
-}
-
