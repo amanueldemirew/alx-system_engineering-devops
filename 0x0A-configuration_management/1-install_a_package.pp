e0012a69c773b28062a26ac85d4b726ba47f2283
Amanuel <amanuel.demirew@yahoo.com>
2023-11-24 18:31:08 +0300

    fix 3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,6 @@
-# install_flask.pp
-
-# Install Flask using pip3
-package { 'python3-pip':
-  ensure => present,
+#!/bin/bin/pup
+# install_flask
+package {'flask':
+ensure   => '2.1.0',
+provider => 'pip3'
 }
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
-}
-
-# Notify that the installation is completed
-notify { 'Flask installation completed':
-  require => Exec['install_flask'],
-}
-
