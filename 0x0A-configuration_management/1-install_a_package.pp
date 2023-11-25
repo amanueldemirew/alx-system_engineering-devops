2f96ea4cc1763928fbf589843237c6afab9a7294
amanuel <amanuel.demirew@yahoo.com>
2023-11-24 22:42:44 -0800

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-# install flask from pip3
-
-package {'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+#Install flask / Version must be 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
