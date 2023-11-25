0aa3f8ab83399ed46b513d5d7abf9f6d24887f24
amanuel <amanuel.demirew@yahoo.com>
2023-11-24 22:48:06 -0800

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,22 @@
-#Install flask / Version must be 2.1.0
+# manifest.pp
+
+# Install the puppetlabs-stdlib module
+class { 'stdlib': }
+
+# Ensure that 'pip3' is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 and pin werkzeug to version 2.0.1 (a version that is compatible with Flask 2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
