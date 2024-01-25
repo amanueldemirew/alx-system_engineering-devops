2399ac1b2d6550619b9c23183a78ea40a1ddbbd5
Amanuel <amanuel.demirew@yahoo.com>
2024-01-25 23:31:47 +0300

    trial

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,27 +1,12 @@
-# Puppet manifest to set ULIMIT variable in /etc/default/nginx
+# Fix problem of high amount of requests
 
-# Set ULIMIT value
-$ulimit_value = '-n 1048576'
-
-# Define file path
-$file_path = '/etc/default/nginx'
-
-# Ensure the file exists
-file { $file_path:
-  ensure => present,
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-# Set the content of the file
-file_line { 'set_ulimit':
-  path   => $file_path,
-  line   => "ULIMIT=${ulimit_value}",
-  match  => '^ULIMIT=',
-  notify => Exec['reload_nginx'],
-}
-
-# Reload Nginx service when the file is updated
-exec { 'reload_nginx':
-  command     => '/usr/sbin/service nginx reload',
-  refreshonly => true,
-  subscribe   => File[$file_path],
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
